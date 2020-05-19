48013333a0ada2d6b7c5bb75897020d60c0250d0
Steve Coleman-Williams <steve@archronos.com>
2020-05-19 15:17:49 +0100

    bug fix

--- src/services/hat.ts
+++ src/services/hat.ts
@@ -20,7 +20,7 @@ class HatService {
     text(text: string, stateCheck: number): void {
         sense.showMessage(text, 0.1, COLORS.PURPLE, () => {
             if (stateSvc.state === stateCheck)
-                this.text(text);
+                this.text(text, stateCheck);
             else
                 this.color([0, 255, 0]);
         });
