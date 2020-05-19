63792bf9c3e022ef8598155fd49dc0b7ac908548
Steve Coleman-Williams <steve@archronos.com>
2020-05-19 13:14:07 +0100

    es6 typo

--- src/controllers/busy.ts
+++ src/controllers/busy.ts
@@ -6,7 +6,7 @@ export default (app: Application) => {
     app.get('/busy', (req: Request, res: Response) => {
         hatSvc.color(COLORS.RED);
         hatSvc.schedule('1:0,255,0', Date.now() + 10000);
-        stateSvc.set(StateService.UNAVAILABLE);
+        stateSvc.state = StateService.UNAVAILABLE;
         res.end();
     })
 }
