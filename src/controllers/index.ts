7f848bfe68a9cab1e0e3ed0342da8c7b539e9138
Steve Coleman-Williams <steve@archronos.com>
2020-05-21 15:22:42 +0100

    added tentative

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -3,10 +3,12 @@ import available from './available';
 import busy from './busy';
 import state from './state';
 import wanking from './wanking';
+import tentative from './tentative';
 
 export default (app: Application) => {
     available(app);
     busy(app);
     state(app);
     wanking(app);
+    tentative(app);
 }
